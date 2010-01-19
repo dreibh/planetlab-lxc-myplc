6469aed9563248b2d3c78a1b48ad35f0b4d343a3
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-01-19 21:25:25 +0000

    moving conf_files creation scripts to nodeconfig

--- build.sh
+++ build.sh
@@ -14,48 +14,39 @@
 
 . build.functions
 
-# XXX .spec file needs to have the appropriate set of requires statements
-
 # Install configuration scripts
-echo "* myplc-native: Installing configuration scripts"
+echo "* Installing configuration scripts"
 pldistro=$1; shift
 RPM_BUILD_ROOT=$1 ; shift
 PYTHON_SITEARCH=`python -c 'from distutils.sysconfig import get_python_lib; print get_python_lib(1)'`
 rm -rf ${RPM_BUILD_ROOT}
 mkdir -p ${RPM_BUILD_ROOT}
-echo "* myplc-native: installing plc_config.py in " ${PYTHON_SITEARCH}
-install -D -m 755 plc_config.py ${RPM_BUILD_ROOT}/${PYTHON_SITEARCH}/plc_config.py
-install -D -m 644 bashrc ${RPM_BUILD_ROOT}/usr/share/myplc/bashrc
 
-echo "* myplc-native: installing scripts in /usr/bin"
-install -D -m 755 plc-config ${RPM_BUILD_ROOT}/usr/bin/plc-config
-install -D -m 755 plc-config-tty ${RPM_BUILD_ROOT}/usr/bin/plc-config-tty
-install -D -m 755 db-config ${RPM_BUILD_ROOT}/usr/bin/db-config
-install -D -m 755 dns-config ${RPM_BUILD_ROOT}/usr/bin/dns-config
-install -D -m 755 plc-map.py ${RPM_BUILD_ROOT}/usr/bin/plc-map.py
-install -D -m 755 plc-kml.py ${RPM_BUILD_ROOT}/usr/bin/plc-kml.py
-install -D -m 755 refresh-peer.py ${RPM_BUILD_ROOT}/usr/bin/refresh-peer.py
-install -D -m 755 clean-empty-dirs.py ${RPM_BUILD_ROOT}/usr/bin/clean-empty-dirs.py
-install -D -m 755 mtail.py ${RPM_BUILD_ROOT}/usr/bin/mtail.py
-install -D -m 755 plc-check-ssl-peering.py ${RPM_BUILD_ROOT}/usr/bin/plc-check-ssl-peering.py
-install -D -m 755 plc-orphan-accounts.py ${RPM_BUILD_ROOT}/usr/bin/plc-orphan-accounts.py
+echo "* Installing plc_config.py in " ${PYTHON_SITEARCH}
+install -D -m 755 plc_config.py ${RPM_BUILD_ROOT}/${PYTHON_SITEARCH}/plc_config.py
+
+echo "* Installing scripts in /usr/bin"
+mkdir -p ${RPM_BUILD_ROOT}/usr/bin
+rsync -av --exclude .svn bin/ ${RPM_BUILD_ROOT}/usr/bin/
+chmod 755 ${RPM_BUILD_ROOT}/usr/bin/*
 
 # Install initscript 
+echo "* Installing plc initscript"
 install -D -m 755 plc.init ${RPM_BUILD_ROOT}/etc/init.d/plc
 
 # Install initscripts
-echo "* myplc-native: Installing initscripts"
+echo "* Installing plc.d initscripts"
 find plc.d | cpio -p -d -u ${RPM_BUILD_ROOT}/etc/
 chmod 755 ${RPM_BUILD_ROOT}/etc/plc.d/*
 
 # Install db-config.d files
-echo "* myplc: Installing db-config.d files"
+echo "* Installing db-config.d files"
 mkdir -p ${RPM_BUILD_ROOT}/etc/planetlab/db-config.d
 cp db-config.d/* ${RPM_BUILD_ROOT}/etc/planetlab/db-config.d
 chmod 444 ${RPM_BUILD_ROOT}/etc/planetlab/db-config.d/*
 
 # Extra scripts (mostly for mail and dns) not installed by myplc by default.  Used in production
-echo "* myplc-native: installing scripts in /etc/support-scripts"
+echo "* Installing scripts in /etc/support-scripts"
 mkdir -p ${RPM_BUILD_ROOT}/etc/support-scripts
 cp support-scripts/* ${RPM_BUILD_ROOT}/etc/support-scripts
 chmod 444 ${RPM_BUILD_ROOT}/etc/support-scripts/*
@@ -69,6 +60,9 @@ chmod 444 ${RPM_BUILD_ROOT}/etc/plc_sliceinitscripts/*
 echo "* myplc: Installing configuration file"
 install -D -m 444 default_config.xml ${RPM_BUILD_ROOT}/etc/planetlab/default_config.xml
 install -D -m 444 plc_config.dtd ${RPM_BUILD_ROOT}/etc/planetlab/plc_config.dtd
+
+echo "* Installing bashrc convenience"
+install -D -m 644 bashrc ${RPM_BUILD_ROOT}/usr/share/myplc/bashrc
 
 # yumgroups.xml and yum repo : let noderepo handle that
 
