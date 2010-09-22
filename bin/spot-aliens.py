d49a8b68b20c91232ba845440fecfc3070b0a389
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2010-09-22 08:46:14 +0200

    checks slices

--- bin/spot-aliens.py
+++ bin/spot-aliens.py
@@ -4,17 +4,25 @@ all_sites=GetSites({},['peer_id','site_id','login_base','name'])
 all_nodes=GetNodes({},['peer_id','node_id','site_id','hostname'])
 all_persons=GetPersons({},['peer_id','person_id','site_ids','key_ids','email'])
 all_keys=GetKeys({},['peer_id','key_id'])
+all_slices=GetSlices({},['peer_id','slice_id','name','site_id'])
 
 site_hash=dict ( [ (site['site_id'],site) for site in all_sites ] )
 #node_hash=dict ( [ (node['node_id'],node) for node in all_nodes ] )
 #person_hash=dict ( [ (person['person_id'],person) for person in all_persons ] )
 key_hash=dict ( [ (key['key_id'],key) for key in all_keys ] )
+slice_hash=dict ( [ (slice['slice_id'],slice) for slice in all_slices ] )
 
 # nodes are expected to be in the same peer as their owning site
 for node in all_nodes:
     site=site_hash[node['site_id']]
     if node['peer_id'] != site['peer_id']: 
         print "NODE-SITE mismatch %r IN SITE %r"%(node,site)
+
+# same for slices
+for slice in all_slices:
+    site=site_hash[slice['site_id']]
+    if slice['peer_id'] != site['peer_id']: 
+        print "SLICE-SITE mismatch %r IN SITE %r"%(slice,site)
 
 # same for persons
 for person in all_persons:
